d565d624aebc1372e2a0e77e42b0fd79719e8255
Yash Koparde <yashkoparde2022@gmail.com>
2026-04-19 21:27:09 +0000

    feat(windows): write main Electron entry process with frameless window setup

--- windows-app/main.js
+++ windows-app/main.js
@@ -1,14 +1,22 @@
 const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
+
 let mainWindow;
+
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 400, height: 600, frame: false, transparent: true, alwaysOnTop: true,
-    webPreferences: { preload: path.join(__dirname, 'preload.js') }
+    width: 400,
+    height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
   });
   mainWindow.loadURL('http://localhost:5173');
-  globalShortcut.register('Space', () => {
-    mainWindow.webContents.send('shortcut-trigger');
-  });
 }
+
 app.whenReady().then(createWindow);
