50fc7c7b8abfa112cd9dc4f18db73f07113866a2
Yash Koparde <yashkoparde2022@gmail.com>
2026-04-20 11:45:08 +0000

    feat(windows): register global hotkey listener for background recording toggle

--- windows-app/main.js
+++ windows-app/main.js
@@ -1,22 +1,14 @@
 const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
-
 let mainWindow;
-
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 400,
-    height: 600,
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: false,
-      contextIsolation: true
-    }
+    width: 400, height: 600, frame: false, transparent: true, alwaysOnTop: true,
+    webPreferences: { preload: path.join(__dirname, 'preload.js') }
   });
   mainWindow.loadURL('http://localhost:5173');
+  globalShortcut.register('Space', () => {
+    mainWindow.webContents.send('shortcut-trigger');
+  });
 }
-
 app.whenReady().then(createWindow);
